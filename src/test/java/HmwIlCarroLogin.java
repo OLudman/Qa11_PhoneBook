b130cb07858eecb3fa87ecbf65b0a11eff30c2a9
OLudman <olgaludman@gmail.com>
2022-01-22 22:38:52 +0200

    QA11 Homework Ludman Olga

---
+++ src/test/java/HmwIlCarroLogin.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HmwIlCarroLogin {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void init(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://ilcarro.xyz/search");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @Test
+    public void LoginPositiveTest(){
+        WebElement loginBtn = wd.findElement(By.xpath("//*[text()='Log in']"));
+        loginBtn.click();
+        WebElement emailInput = wd.findElement(By.xpath("//input[@id='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("olgaludman@gmail.com");
+        WebElement passInput = wd.findElement(By.xpath("//input[@id='password']"));
+        passInput.click();
+        passInput.clear();
+        passInput.sendKeys("Ol4ik1Ya");
+        wd.findElement(By.cssSelector("button[type='submit']")).click();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        wd.quit();
+    }
+}
